5d0ac552abe00f0b8f77407b352fe30da4d9a47b
Hugo Lopes <hugo.lopes-ext@ledger.fr>
2022-12-01 14:53:18 +0000

    Added debug messages, bound events to provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import Onboard from "@web3-onboard/core";
-
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { VStack, Button, Text, HStack } from "@chakra-ui/react";
 import { Tooltip } from "@chakra-ui/react";
 import injectedModule from "@web3-onboard/injected-wallets";
-import ledgerModule from '@web3-onboard/ledger'
+import ledgerModule from '@web3-onboard/ledger';
+import walletConnectModule from '@web3-onboard/walletconnect';
 
-const MAINNET_RPC_URL = `https://cloudflare-eth.com`;
+import Onboard from "@web3-onboard/core";
 
 export const shortenAddress = (address) => {
   if (!address) return "No Account";
@@ -17,61 +16,145 @@ export const shortenAddress = (address) => {
   return `${match[1]}…${match[2]}`;
 };
 
-const ledger = ledgerModule();
+const ledger = ledgerModule({
+  chainId: 1,
+  rpc: {
+    1: `https://cloudflare-eth.com/`, // Mainnet
+    5: 'https://goerli.optimism.io',  // Goerli
+    137: "https://polygon-rpc.com/",  // Polygon
+  }
+});
 const injected = injectedModule();
+const walletConnect = walletConnectModule();
+
+const wallets = [injected, ledger, walletConnect];
+const chains = [
+  {
+    id: "0x1", // chain ID must be in hexadecimal
+    token: "ETH", // main chain token
+    namespace: "evm",
+    label: "Ethereum Mainnet",
+    rpcUrl: 'https://cloudflare-eth.com',
+  },
+  {
+    id: "0x5", // chain ID must be in hexadecimal
+    token: "ETH", // main chain token
+    namespace: "evm",
+    label: "Ethereum Goerli",
+    rpcUrl: "https://goerli.optimism.io",
+  },
+  {
+    id: "0x89",
+    token: "MATIC",
+    namespace: "evm",
+    label: "Polygon",
+    rpcUrl: "https://polygon-rpc.com/"
+  }
+];
 
 const onboard = Onboard({
-  wallets: [injected, ledger],
-  chains: [
-    {
-      id: "0x1", // chain ID must be in hexadecimel
-      token: "ETH", // main chain token
-      namespace: "evm",
-      label: "Ethereum Mainnet",
-      rpcUrl: MAINNET_RPC_URL
-    }
-  ]
+  wallets,
+  chains,
+  appMetadata: {
+    name: "Ledger web3-Onboard Demo",
+    icon: '<svg>My App Icon</svg>',
+    description: "A demo of Web3-Onboard with Ledger."
+  },
+  // accountCenter: {
+  //   desktop: {
+  //     position: 'topRight',
+  //     enabled: true,
+  //     minimal: false
+  //   },
+  //   // mobile: {
+  //   //   position: 'bottomRight',
+  //   //   enabled: true,
+  //   //   minimal: true
+  //   // }
+  // }
 });
 
+export const toHex = (val) =>
+  typeof val === 'number' ? `0x${val.toString(16)}` : val;
+
 export default function Home() {
-  const [_, setProvider] = useState();
+  const [provider, setProvider] = useState();
   const [account, setAccount] = useState();
   const [chainId, setChainId] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+  const [isConnecting, setIsConnecting] = useState(false);
+
+  const currentState = onboard.state.get()
 
   const connectWallet = async () => {
-    try {
-      const wallets = await onboard.connectWallet();
-      console.log("wallets are", wallets);
+    console.log('>> connectWallet');
+    setIsConnecting(true);
 
-      if (wallets.length) {
-        setIsLoading(true);
-        const { accounts, chains, provider } = wallets[0];
-        if (accounts.length) {
-          console.log("accounts are", accounts);
-          setAccount(accounts[0].address);
-        }
-        setChainId(chains[0].id);
-        setProvider(provider);
-        setIsLoading(false);
+    const wallets = await onboard.connectWallet();
+    console.log(">> wallets are", wallets);
+
+    if (wallets.length) {
+      const { accounts, chains, provider } = wallets[0];
+
+      if (accounts.length) {
+        console.log(">> accounts are", accounts);
+        setAccount(accounts[0].address);
       }
-    } catch (error) {
-      setError(error);
-    }
-  };
+      setChainId(chains[0].id);
+      setProvider(provider);
+    };
+    setIsConnecting(false);
+  }
 
+  // called by handleDisconnect and when disconnecting from wallet
   const disconnect = async () => {
-    const [primaryWallet] = await onboard.state.get().wallets;
-    if (!primaryWallet) return;
-    await onboard.disconnectWallet({ label: primaryWallet.label });
-    refreshState();
-  };
+    console.log('>> dicconnect');
 
-  const refreshState = () => {
-    setAccount("");
-    setChainId("");
+    const [primaryWallet] = onboard.state.get().wallets;
+    if (!primaryWallet) return;
+
+    console.log('>> disconnecting primaryWallet', primaryWallet);
+    await onboard.disconnectWallet({ label: primaryWallet.label });
+
+    setAccount();
+    setChainId();
     setProvider();
   };
+
+  const switchAccount = (accounts) => {
+    console.log('>> accountsChanged', accounts);
+    setAccount(accounts[0]);
+  }
+
+  const switchChain = (chainId) => {
+    console.log('>> chainChanged', chainId);
+    setChainId(chainId);
+  }
+
+  useEffect(() => {
+    console.log('>> useEffect');
+    console.log('>> current state is', currentState);
+
+    if (provider?.on) {
+      console.log('>> assigning provider event handlers');
+
+      provider.on("connect", () => console.log('>> connect event fired'));
+      provider.on("close", (code, reason) => console.log('>> close event fired', code, reason));
+      provider.on("networkChanged", (networkId) => console.log('>> networkChanged event fired', networkId));
+      provider.on("eth_subscription", (result) => console.log('>> eth_subscription event fired', result));
+
+      provider.on("disconnect", disconnect);
+      provider.on("accountsChanged", switchAccount);
+      provider.on("chainChanged", switchChain);
+
+      return () => {
+        if (provider.removeListener) {
+          provider.removeListener("disconnect", disconnect);
+          provider.removeListener("accountsChanged", switchAccount);
+          provider.removeListener("chainChanged", switchChain);
+        }
+      };
+    }
+  }, [provider]);
 
   return (
     <>
@@ -84,10 +167,9 @@ export default function Home() {
             Connect with Web3-Onboard
           </Text>
         </HStack>
-        {isLoading && <div>Loading...</div>}
         <HStack>
           {!account ? (
-            <Button onClick={connectWallet}>Connect Wallet</Button>
+            <Button onClick={connectWallet} disabled={isConnecting}>Connect Wallet</Button>
           ) : (
             <Button onClick={disconnect}>Disconnect</Button>
           )}
